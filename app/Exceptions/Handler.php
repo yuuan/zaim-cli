f65c993d4570dafb8ca0aa4a531f5742cdede9d6
yuuAn <nora@yuuan.net>
2019-06-28 02:37:08 +0900

    例外ハンドラを作成
    - https://laravel-zero.com/docs/exception-handler/

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+
+class Handler extends ExceptionHandler
+{
+    /**
+     * A list of the exception types that should not be reported.
+     *
+     * @var array
+     */
+    protected $dontReport = [
+        // \Illuminate\Database\Eloquent\ModelNotFoundException::class,
+    ];
+
+    /**
+     * Report or log an exception.
+     *
+     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
+     *
+     * @param  \Exception  $exception
+     * @return void
+     */
+    public function report(Exception $exception)
+    {
+        parent::report($exception);
+    }
+}
